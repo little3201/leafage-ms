281970e6ce670e792a946124258bcf2e5668be6f
李文强 <liwenqiang@tseverday.com>
2020-04-14 13:36:50 +0800

    后台框架搭建

--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
+// =========================================================
+// * Vuetify Material Dashboard - v2.1.0
+// =========================================================
+//
+// * Product Page: https://www.creative-tim.com/product/vuetify-material-dashboard
+// * Copyright 2019 Creative Tim (https://www.creative-tim.com)
+//
+// * Coded by Creative Tim
+//
+// =========================================================
+//
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// element-ui
-import ElementUI from 'element-ui'
-import './element.scss'
+import './plugins/base'
+import './plugins/chartist'
+import './plugins/vee-validate'
+import vuetify from './plugins/vuetify'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
-
-Vue.use(ElementUI)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  vuetify,
+  i18n,
+  render: h => h(App),
 }).$mount('#app')

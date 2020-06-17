f8488dc3a79eb0870ddd2d1fa390e5b66b809d74
little3201 <little3201@163.com>
2020-06-17 23:52:35 +0800

    编辑eidt.vue

--- src/views/article/Edit.vue
+++ src/views/article/Edit.vue
@@ -1,5 +1,13 @@
 <template>
   <v-container fluid class="full-height">
+    <v-row>
+      <v-text-field label="Title" single-line></v-text-field>
+    </v-row>
+    <v-row>
+      <v-textarea auto-grow label="Content" solo flat></v-textarea>
+      <v-divider vertical></v-divider>
+      <v-textarea auto-grow label="Content" disabled></v-textarea>
+    </v-row>
     <v-row justify="center" align="start" class="my-0">
       <v-md-editor v-model="details.statusText" height="600px"></v-md-editor>
     </v-row>

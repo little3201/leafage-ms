7ff87404076e125c2d606e9f8d90d3cd315873b8
little3201 <little3201@163.com>
2020-07-28 07:32:54 +0800

    菜单排版修改

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -41,25 +41,59 @@
     >
 
       <template v-for="(item, i) in computedItems">
-        <v-item-group
+        <v-list-group
           v-if="item.children"
           :key="`group-${i}`"
-          :subGroup="item.children"
+          prepend-icon="mdi-account-multiple"
+          value="true"
         >
-          <v-list-item
-            :key="`item-${i}`"
-            :to="item.to"
+          <template v-slot:activator>
+            <v-list-item-title>Manager</v-list-item-title>
+          </template>
+
+          <v-list-group
+            no-action
+            sub-group
+            value="true"
           >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>User</v-list-item-title>
+              </v-list-item-content>
+            </template>
 
-            <v-list-item-icon>
-              <v-icon v-text="item.icon" />
-            </v-list-item-icon>
+            <v-list-item
+              v-for="(admin, i) in admins"
+              :key="i"
+              link
+            >
+              <v-list-item-title v-text="admin[0]"></v-list-item-title>
+              <v-list-item-icon>
+                <v-icon v-text="admin[1]"></v-icon>
+              </v-list-item-icon>
+            </v-list-item>
+          </v-list-group>
 
-            <v-list-item-content>
-              <v-list-item-title v-text="item.title" />
-            </v-list-item-content>
-          </v-list-item>
-        </v-item-group>
+          <v-list-group
+            sub-group
+            no-action
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Actions</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item
+              v-for="(crud, i) in cruds"
+              :key="i"
+            >
+              <v-list-item-title v-text="crud[0]"></v-list-item-title>
+              <v-list-item-action>
+                <v-icon v-text="crud[1]"></v-icon>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-group>
+        </v-list-group>
 
         <v-list-item
           v-else
@@ -130,6 +164,16 @@ export default {
         icon: 'mdi-book-open-page-variant',
         to: '/article'
       }
+    ],
+    admins: [
+      ['Management', 'mdi-people-outline'],
+      ['Settings', 'mdi-settings']
+    ],
+    cruds: [
+      ['Create', 'add'],
+      ['Read', 'insert_drive_file'],
+      ['Update', 'update'],
+      ['Delete', 'delete']
     ]
   }),
 
--- src/views/article/Item.vue
+++ src/views/article/Item.vue
@@ -11,56 +11,6 @@
           :headers="headers"
           :items="items"
         >
-          <template v-slot:top>
-            <v-toolbar flat>
-              <v-toolbar-title>文章信息</v-toolbar-title>
-              <v-divider
-                class="mx-4"
-                inset
-                vertical
-              ></v-divider>
-              <v-spacer></v-spacer>
-              <v-dialog v-model="dialog" max-width="500px">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    color="primary"
-                    dark
-                    class="mb-2"
-                    v-bind="attrs"
-                    v-on="on"
-                  >新增</v-btn>
-                </template>
-                <v-card>
-                  <v-card-title>
-                    <span class="headline">{{ formTitle }}</span>
-                  </v-card-title>
-
-                  <v-card-text>
-                    <v-container>
-                      <v-row>
-                        <v-col cols="12" md="6">
-                          <v-text-field outlined dense single-line label="Title"></v-text-field>
-                          <v-text-field outlined dense single-line label="Subtitle"></v-text-field>
-                          <v-text-field outlined dense single-line label="Title"></v-text-field>
-                        </v-col>
-                        <v-col cols="12" md="6">
-                          <v-text-field outlined dense single-line label="Title"></v-text-field>
-                          <v-text-field outlined dense single-line label="Subtitle"></v-text-field>
-                          <v-text-field outlined dense single-line label="Title"></v-text-field>
-                        </v-col>
-                      </v-row>
-                    </v-container>
-                  </v-card-text>
-
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                    <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-toolbar>
-          </template>
           <template v-slot:item.actions="{ item }">
             <v-icon
               small

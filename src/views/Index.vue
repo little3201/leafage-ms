4d904034a0c23280d921c134b7d2ed7f22016743
李文强 <liwenqiang@tseverday.com>
2020-08-27 14:48:02 +0800

    统计月更新

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,19 @@
 <template>
   <v-container fluid>
-    <!-- <v-row> -->
-      <v-sparkline
-        :labels="labels"
-        :value="value"
-        :gradient="gradient"
-        smooth
-        line-width="1"
-        auto-draw
-        show-labels
-        label-size="4"
-      ></v-sparkline>
-    <!-- </v-row> -->
+    <v-row>
+      <v-col md="6">
+        <v-sparkline
+          :labels="labels"
+          :value="value"
+          :gradient="gradient"
+          smooth
+          line-width="1"
+          auto-draw
+          show-labels
+          label-size="4"
+        ></v-sparkline>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -45,9 +47,8 @@ export default {
     monthCount () {
       axios.get(SERVER_URL.article.concat('/statistics')).then(response => {
         if (response.data) {
-          debugger
-          this.labels = [...this.labels, ...response.data.label]
-          this.value = [...this.value, ...response.data.value]
+          this.labels = [...this.labels, ...response.data.map(item => item.label)]
+          this.value = [...this.value, ...response.data.map(item => item.value)]
         }
       }).catch(error => {
         alert(error.statusText)

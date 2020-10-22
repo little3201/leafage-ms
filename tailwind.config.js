08a2656f3183ad2074a3a9f431aa468b366c45a0
李文强 <liwenqiang@tseverday.com>
2020-10-22 20:31:50 +0800

    修改tsconfig.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,4 +53,7 @@ module.exports = {
   },
   purge: ['./index.html', './src/**/*.vue', './src/**/*.ts'],
   plugins: [],
+  variants: {
+    animation: ['responsive', 'motion-safe', 'motion-reduce']
+  }
 }

ae5a242274b053ad38d8b3156805dcad54ce2e87
little3201 <little3201@163.com>
2021-01-25 15:56:59 +0800

    修改展开合并图标

--- src/components/global/SideMenu.vue
+++ src/components/global/SideMenu.vue
@@ -64,18 +64,33 @@
           <div class="hidden xl:flex items-center w-full">
             System
             <svg
+              v-if="isSystemOpen"
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="1.5"
+              stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
               class="feather feather-chevron-down ml-auto mr-2"
             >
               <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
+            <svg v-else
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-chevron-right ml-auto mr-2"
+            >
+              <polyline points="9 18 15 12 9 6"></polyline>
             </svg>
           </div>
         </a>

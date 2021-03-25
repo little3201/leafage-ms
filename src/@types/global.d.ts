fb6f894257ae99a0bb20f914cc40bb1ce5ee87e8
little3201 <little3201@163.com>
2021-03-25 18:26:13 +0800

    解决问题

---
+++ src/@types/global.d.ts
@@ -0,0 +1,13 @@
+declare module 'qs' {
+    import qs from 'qs'
+    export default qs
+}
+
+declare module 'highlight.js/lib/core' {
+    import hljs from 'highlight.js/lib/core'
+    export default hljs
+}
+
+declare module 'highlight.js/lib/languages/*' {
+    export default function (hljs?: HLJSApi): LanguageDetail;
+}

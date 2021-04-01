937f035a7ebe6a3f6178f659fdfe6b2b5c56dc94
little3201 <little3201@163.com>
2021-04-01 19:40:43 +0800

    添加chart.js示例

---
+++ src/components/global/Doughnut.vue
@@ -0,0 +1,55 @@
+<template>
+  <canvas id="doughnut" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Chart from "chart.js";
+
+export default defineComponent({
+  name: "Doughnut",
+
+  props: {
+    options: {
+      type: Object,
+      default: {},
+    },
+    labels: {
+      type: Array,
+      default: [],
+    },
+    colors: {
+      type: Array,
+      default: [],
+    },
+    data: {
+      type: Array,
+      default: [],
+    },
+  },
+
+  mounted() {
+    this.createChart({
+      datasets: [
+        {
+          data: this.data,
+          backgroundColor: this.colors,
+        },
+      ],
+      labels: this.labels,
+    });
+  },
+
+  methods: {
+    createChart(chartData: object) {
+      const canvas = document.getElementById("doughnut") as HTMLCanvasElement;
+      const options = {
+        type: "doughnut",
+        data: chartData,
+        options: this.options,
+      };
+      new Chart(canvas, options);
+    },
+  },
+});
+</script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,29 +1,35 @@
 <template>
-  <Report />
+  <div>
+    <Report />
+    <Doughnut
+      :data="[25, 75, 30]"
+      :labels="['Red', 'Green', 'Blue']"
+      :colors="['red', 'green', 'blue']"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from "vue";
 import Report from "/@/components/global/Report.vue";
+import Doughnut from "/@/components/global/Doughnut.vue";
 
 export default defineComponent({
   name: "Dashboard",
 
   components: {
     Report,
+    Doughnut,
   },
 
   setup() {
-
-    async function initList() {
-    }
+    async function initList() {}
 
     onMounted(() => {
       initList();
     });
 
-    return {
-    };
+    return {};
   },
 });
 </script>

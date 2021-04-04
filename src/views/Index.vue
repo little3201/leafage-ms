b60087af55baef4b7a83962d0b9d5256b8c71cf9
liwenqiang <little3201@163.com>
2021-04-04 16:56:29 +0800

    删除index.vue请求数据的方法

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -5,42 +5,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent } from "vue";
 import Report from "/@/components/global/Report.vue";
-
-import instance from "../api";
-import SERVER_URL from "../api/request";
 
 export default defineComponent({
   name: "Dashboard",
 
   components: {
     Report,
-  },
-
-  setup() {
-    let data = ref<Array<Object>>(new Array<Object>());
-    let name = ref("Category");
-    const labels = ref<Array<String>>(new Array<String>());
-
-    function initList() {
-      instance.get(SERVER_URL.category).then((res) => {
-        res.data.forEach((e: any) => {
-          labels.value.push(e.alias);
-          data.value.push(e.count);
-        });
-      });
-    }
-
-    onMounted(() => {
-      initList();
-    });
-
-    return {
-      name,
-      data,
-      labels
-    };
   },
 });
 </script>

03fd5387cc8db8371bc0d514ba422dfb12132882
little3201 <little3201@163.com>
2021-04-08 10:20:45 +0800

    修改高亮样式，去掉markdown.css使用tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  mode: 'jit',
   darkMode: 'media', // or 'media' or 'class'
   purge: ['./index.html', './src/**/*.vue', './src/**/*.ts'],
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
   variants: {
   },

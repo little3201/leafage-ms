78b6a74afd822c4d464e860522a168ac2e36ee70
liwenqiang <little3201@163.com>
2021-06-13 10:48:54 +0800

    vite-plugin-mock插件卸载

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,4 @@ import './assets/main.css'
 import router from './router'
 import { store, key } from './store'
 
-import { setupProdMockServer } from './plugins/mockServer';
-
-// dev mock server
-if (process.env.NODE_ENV != 'production') {
-    setupProdMockServer();
-    sessionStorage.setItem("user", JSON.stringify({ username: "leafage", avatar: "/images/avatar.jpg", nickname: "布吉岛" }))
-}
-
 createApp(App).use(router).use(store, key).mount('#app')

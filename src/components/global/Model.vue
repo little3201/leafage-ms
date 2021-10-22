17c5099632558e3e5b07d276b3fe4913511ca694
little3201 <little3201@163.com>
2021-10-22 17:41:20 +0800

    model问题修改

--- src/components/global/Model.vue
+++ src/components/global/Model.vue
@@ -1,26 +1,24 @@
 <template>
   <transition name="model">
-    <div
-      v-show="isShow"
-      class="fixed inset-0 bg-black bg-opacity-50 transition-opacity"
-      role="dialog"
-    >
+    <div v-show="isShow" class="fixed inset-0 overflow-y-auto" role="dialog">
       <div
-        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-10 text-center sm:block sm:p-0"
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
       >
-        <span class="inline-block align-top sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <div class="fixed inset-0 bg-black bg-opacity-50 transition-opacity" aria-hidden="true"></div>
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
         <div
           class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle max-w-2xl"
         >
-          <div class="bg-white p-4 sm:p-6 max-h-96 overflow-auto sm:max-h-full">
-            <slot></slot>
+          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div class="sm:flex sm:items-start">
+              <slot></slot>
+            </div>
           </div>
-          <!--footer-->
           <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse text-sm">
             <button
               type="button"
               @click="commitOperation"
-              class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 font-medium text-white focus:outline-none sm:ml-3 sm:w-auto active:cursor-wait"
+              class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 font-medium text-white focus:outline-none focus:ring-1 focus:ring-offset-2 sm:ml-3 sm:w-auto active:cursor-wait"
               :class="[
                 isConfirm
                   ? 'bg-red-600 hover:bg-red-700 focus:ring-red-500'
@@ -30,7 +28,7 @@
             <button
               type="button"
               @click="cancelOperation"
-              class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto active:cursor-wait"
+              class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto active:cursor-wait"
             >取消</button>
           </div>
         </div>

51287b261394739ca64cef217ca7d1b058b0e545
liwenqiang <little3201@163.com>
2022-11-14 21:37:29 +0800

    添加rehype-external-links

--- src/plugins/markdownToHtml.ts
+++ src/plugins/markdownToHtml.ts
@@ -1,7 +1,9 @@
-import { remark } from 'remark'
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import remarkGfm from 'remark-gfm'
 import rehypeHighlight from 'rehype-highlight'
+import rehypeExternalLinks from 'rehype-external-links'
 import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
 
@@ -19,9 +21,11 @@ import xml from 'highlight.js/lib/languages/xml';
 import yaml from 'highlight.js/lib/languages/yaml';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark()
+  const result = await unified()
+    .use(remarkParse)
     .use(remarkRehype)
     .use(remarkGfm)
+    .use(rehypeExternalLinks, { target: '_blank', rel: ['nofollow'] })
     .use(rehypeHighlight, { languages: { bash, dockerfile, javascript, handlebars, java, json, nginx, shell, sql, typescript, xml, yaml } })
     .use(rehypeSanitize, {
       ...defaultSchema,

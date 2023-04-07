fac1ba8fa33cda1b66f07b85c0dc086dba668071
liwenqiang <little3201@163.com>
2023-04-07 15:36:52 +0800

    turn off the multi-word-component-names

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     plugins: ['vue', '@typescript-eslint'],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'vue/multi-word-component-names': 'off'
     },
 };

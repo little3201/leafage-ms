1ef23cf15bd072e6ab6af21244f2a760b74e1e09
liwenqiang <little3201@163.com>
2023-12-30 11:13:00 +0800

    init with quasar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,36 @@
-import { createRouter, createWebHistory, RouteLocationNormalized, NavigationGuardNext } from 'vue-router';
+import { route } from 'quasar/wrappers';
+import {
+  createMemoryHistory,
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from 'vue-router';
+
 import routes from './routes';
-import ProgressBar from "@badrap/bar-of-progress";
 
-const progress = new ProgressBar();
+/*
+ * If not building with SSR mode, you can
+ * directly export the Router instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Router instance.
+ */
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
+export default route(function (/* { store, ssrContext } */) {
+  const createHistory = process.env.SERVER
+    ? createMemoryHistory
+    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+
+  const Router = createRouter({
+    scrollBehavior: () => ({ left: 0, top: 0 }),
+    routes,
+
+    // Leave this as is and make changes in quasar.conf.js instead!
+    // quasar.conf.js -> build -> vueRouterMode
+    // quasar.conf.js -> build -> publicPath
+    history: createHistory(process.env.VUE_ROUTER_BASE),
+  });
+
+  return Router;
 });
-
-/* 路由之前 */
-router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
-  progress.start()
-  next()
-})
-
-router.afterEach(() => {
-  progress.finish()
-})
-
-export default router;
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,126 +1,18 @@
 import { RouteRecordRaw } from 'vue-router';
-import Index from '~/views/Index.vue'
 
-import NotFound from '~/views/NotFound.vue'
-
-const routes: Array<RouteRecordRaw> = [
+const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    name: 'Application',
-    component: Index,
-    children: [
-      // 为这2个 URL 匹配
-      // - /
-      // - /dashboard
-      {
-        path: 'dashboard',
-        name: 'Dashboard',
-        alias: '',
-        component: () => import('~/views/Dashboard.vue')
-      },
-      {
-        path: 'system',
-        name: 'System',
-        component: () => import('~/views/system/Index.vue'),
-        redirect: '/system/users',
-        children: [
-          {
-            path: 'users',
-            name: 'Users',
-            component: () => import('~/views/system/User.vue'),
-            meta: { requiresAuth: true }
-          },
-          {
-            path: 'groups',
-            name: 'Groups',
-            component: () => import('~/views/system/Group.vue'),
-            meta: { requiresAuth: true }
-          },
-          {
-            path: 'roles',
-            name: 'Roles',
-            component: () => import('~/views/system/Role.vue'),
-            meta: { requiresAuth: true }
-          },
-          {
-            path: 'components',
-            name: 'Components',
-            component: () => import('~/views/system/Component.vue'),
-            meta: { requiresAuth: true }
-          },
-          {
-            path: 'dictionaries',
-            name: 'Dictionaries',
-            component: () => import('~/views/system/Dictionary.vue')
-          }
-        ]
-      },
-      {
-        path: 'posts',
-        name: 'Posts',
-        component: () => import('~/views/assets/Posts.vue')
-      },
-      {
-        path: 'categories',
-        name: 'Categories',
-        component: () => import('~/views/assets/Category.vue')
-      },
-      {
-        path: 'regions',
-        name: 'Regions',
-        component: () => import('~/views/assets/Region.vue')
-      },
-      {
-        path: '/settings',
-        name: 'Settings',
-        component: () => import('~/views/settings/Index.vue'),
-        children: [
-          {
-            path: 'account',
-            name: 'Account',
-            alias: '',
-            component: () => import('~/views/settings/Account.vue'),
-            meta: { requiresAuth: true }
-          },
-          {
-            path: 'profile',
-            name: 'Profile',
-            component: () => import('~/views/settings/Profile.vue'),
-            meta: { requiresAuth: true }
-          },
-          {
-            path: 'security',
-            name: 'Security',
-            component: () => import('~/views/settings/Security.vue'),
-            meta: { requiresAuth: true }
-          },
-          {
-            path: 'messages',
-            name: 'Messages',
-            component: () => import('~/views/settings/Message.vue'),
-            meta: { requiresAuth: true }
-          },
-          {
-            path: 'accesslogs',
-            name: 'Accesslogs',
-            component: () => import('~/views/settings/Accesslog.vue'),
-            meta: { requiresAuth: true }
-          }
-        ]
-      }
-    ]
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
+
+  // Always leave this as last one,
+  // but you can also remove it
   {
-    path: '/signin',
-    name: 'SignIn',
-    component: () => import('~/views/sign/SignIn.vue'),
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
   },
-  {
-    path: '/signup',
-    name: 'Signup',
-    component: () => import('~/views/sign/SignUp.vue'),
-  },
-  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ];
 
 export default routes;

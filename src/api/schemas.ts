bbf97852359275da3fcd78fac45de680c87140ba
liwenqiang <little3201@163.com>
2024-11-26 18:24:17 +0800

    修改exploiters

--- src/api/schemas.ts
+++ src/api/schemas.ts
@@ -4,8 +4,8 @@ import type { Pagination, Schema, Field } from 'src/models'
 
 /**
  * Retrieve rows
- * @param page Page number
- * @param size Items per page
+ * @param pagination Pagination and sort parameters
+ * @param filters Optional filter or sort parameters
  * @param filters Optional filter or sort parameters
  * @returns Rows data
  */
